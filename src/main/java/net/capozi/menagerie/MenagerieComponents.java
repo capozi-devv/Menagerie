2ad26846accdc9f69c5fc37bb3c1be6d2f07d757
Capozi <capozi.devv@gmail.com>
2025-11-09 21:15:20 -0500

    without even reasoning why
    fixed all the bugs

--- src/main/java/net/capozi/menagerie/MenagerieComponents.java
+++ src/main/java/net/capozi/menagerie/MenagerieComponents.java
@@ -5,22 +5,15 @@ import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
-import dev.onyxstudios.cca.api.v3.world.WorldComponentFactoryRegistry;
-import dev.onyxstudios.cca.api.v3.world.WorldComponentInitializer;
 import net.capozi.menagerie.common.network.*;
 import net.minecraft.util.Identifier;
 
-public class MenagerieComponents implements EntityComponentInitializer, WorldComponentInitializer {
+public class MenagerieComponents implements EntityComponentInitializer {
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
         Menagerie.BOUND_ARTIFACT = ComponentRegistry.getOrCreate(new Identifier(Menagerie.MOD_ID, "bound_artifact"), BoundArtifactComponent.class);
         Menagerie.BOUND_ACCURSED = ComponentRegistry.getOrCreate(new Identifier(Menagerie.MOD_ID, "bound_accursed"), BoundAccursedComponent.class);
         registry.registerForPlayers(Menagerie.BOUND_ARTIFACT, player -> (BoundArtifactComponent) new BoundArtifactComponentImpl(), RespawnCopyStrategy.ALWAYS_COPY);
         registry.registerForPlayers(Menagerie.BOUND_ACCURSED, player -> (BoundAccursedComponent) new BoundAccursedComponentImpl(), RespawnCopyStrategy.ALWAYS_COPY);
-    }
-
-    @Override
-    public void registerWorldComponentFactories(WorldComponentFactoryRegistry registry) {
-
     }
 }
